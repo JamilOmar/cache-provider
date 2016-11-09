2a06fba0442349be2843ed61bc9a9a632a54d252
jamilomar <jamilomar.falconiaguirre@gmail.com>
2016-11-09 17:11:10 -0500

    PubSub added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-var cache =  require('./lib/cache');
+const cache =  require('./lib/cache');
+const pubSub = require('./lib/pubSub');
 const logger = {
 
 error :function (error)
@@ -21,9 +22,49 @@ logger:logger,
 type :"redis-provider"
 
 };
-const cobject = new cache(options);
-cobject.initialize();
 
+let pubSubOptions = {
+
+connection : { "host":"127.0.0.1",
+          "port":6379 
+},configuration:
+    {
+    subscriber :true
+    },
+logger:logger,
+list :"list"
+};
+
+let pubSubOptionsPub = {
+
+connection : { "host":"127.0.0.1",
+          "port":6379 
+},
+configuration:
+    {
+    publisher :true
+    },
+logger:logger,
+list :"list"
+};
+
+////test for cache 
+var cobject = new cache(options);
+var cpubSub = new pubSub(pubSubOptions);
+var cpubSubPub = new pubSub(pubSubOptionsPub);
+
+cobject.initialize();
+/// test for pubSub
+cpubSub.initialize();
+cpubSubPub.initialize();
+cpubSub.on("message",(channel,message)=>
+{
+    console.log(message);
+})
+cpubSub.on("subscribe",(channel,count)=>
+{
+    console.log(count);
+})
 app.get('/',  (req, res)=> {
 
 
@@ -36,7 +77,6 @@ app.get('/',  (req, res)=> {
 
 
 })
-
 app.post('/insert',(req, res) => {
   cobject.saveCache(["ID","Jamil","INFO"],{name:"jamil", age:31}, (err,data)=>
   {
@@ -44,6 +84,10 @@ app.post('/insert',(req, res) => {
           res.send("success");
 
   })
+})
+app.post('/publish',(req, res) => {
+    cpubSubPub.publish("using pubsub");
+     res.send("using pubsub");
 })
 
 
